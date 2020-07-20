8adcd1435e69286cb9192a0317da007de3ec8475
Jennifer Tran <jennantran@gmail.com>
2020-07-19 22:31:23 -0700

    Updates

---
+++ precache-manifest.d69bb958767b9d2e763e1705f17ddf52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53afbf643d7b190612586ba66eccfab6",
+    "url": "/motionlogs-app/index.html"
+  },
+  {
+    "revision": "7a58e073b7bf77802153",
+    "url": "/motionlogs-app/static/css/main.ce74c24e.chunk.css"
+  },
+  {
+    "revision": "a0c2a5822257205e9012",
+    "url": "/motionlogs-app/static/js/2.6d46bd64.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/motionlogs-app/static/js/2.6d46bd64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a58e073b7bf77802153",
+    "url": "/motionlogs-app/static/js/main.6cddd0f7.chunk.js"
+  },
+  {
+    "revision": "116f97d7b3ce6d3384f7",
+    "url": "/motionlogs-app/static/js/runtime-main.58c8ad0d.js"
+  },
+  {
+    "revision": "406d66a62a160c60047a62f67b0b568e",
+    "url": "/motionlogs-app/static/media/barbellshadow.406d66a6.jpg"
+  }
+]);
