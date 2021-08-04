9fcc74306d329b48a5babb8c6a2eaeab5b2eb66a
Jennifer Tran <jennantran@gmail.com>
2021-08-04 18:53:13 -0500

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default{
-    API_ENDPOINT: 'https://sheltered-fortress-79630.herokuapp.com/api'
-    ,
+    API_ENDPOINT: 'https://sheltered-fortress-79630.herokuapp.com/api',
     TOKEN_KEY: 'logs-client-auth-token',
 }
